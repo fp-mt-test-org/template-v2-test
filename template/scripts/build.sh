92ae477f0926d2f7d9f7737620c2f84dbfa7450c
Matthew Thomas <matthewthomas@flexport.com>
2021-07-18 00:17:59 -0700

    norm

--- template/scripts/build.sh
+++ template/scripts/build.sh
@@ -6,23 +6,12 @@ set -o nounset
 
 brew install jfrog-cli
 
-jfrog_output=$(jfrog config show artifactory 2>&1 || true)
-does_not_exist_pattern='does not exist'
-
-echo "jfrog_output:"
-echo "${jfrog_output}"
-echo 
-
-if [[ ${jfrog_output} =~ ${does_not_exist_pattern} ]]; then
-    echo "artifactory doesn't exist, adding ${artifactory_base_url}"
-
-    jfrog c add artifactory \
-        --url="${artifactory_base_url}" \
-        --user="${artifactory_username}" \
-        --access-token="${artifactory_password}" \
-        --artifactory-url="${artifactory_base_url}/artifactory" \
-        --interactive=false
-fi
+jfrog c add artifactory \
+    --url="${artifactory_base_url}" \
+    --user="${artifactory_username}" \
+    --access-token="${artifactory_password}" \
+    --artifactory-url="${artifactory_base_url}/artifactory" \
+    --interactive=false
 
 jfrog rt \
     gradle clean \
