86e642364e547d400cde2e63d318d4fbb6d5eb54
Matthew Thomas <matthewthomas@flexport.com>
2021-07-17 23:52:06 -0700

    add

--- template/scripts/build.sh
+++ template/scripts/build.sh
@@ -10,6 +10,8 @@ jfrog_output=$(jfrog config show artifactory 2>&1 || true)
 does_not_exist_pattern='does not exist'
 
 if [[ ${jfrog_output} =~ ${does_not_exist_pattern} ]]; then
+    echo "artifactory doesn't exist, adding ${artifactory_base_url}"
+
     jfrog c add artifactory \
         --url="${artifactory_base_url}" \
         --user="${artifactory_username}" \
