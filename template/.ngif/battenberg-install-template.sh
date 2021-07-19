a9d2c0da39b4aa2f6060f1df7e126251d6d7215b
Matthew Thomas <matthewthomas@flexport.com>
2021-07-18 23:18:53 -0700

    these two?

--- template/.ngif/battenberg-install-template.sh
+++ template/.ngif/battenberg-install-template.sh
@@ -24,43 +24,12 @@ echo
         sleep 1;
     fi
 
-    # owner [Product Infrastructure]:
-    jq -r '.owner' ${template_context_path};
-    sleep 1;
-
     # component_id []:
     jq -r '.component_id' ${template_context_path}
-    sleep 1;
-
-    # repo_owner [fp-mt-test-org]:
-    jq -r '.repo_owner' ${template_context_path}
-    sleep 1;
-
-    # artifact_id [java-kotlin-lib-test-*****]:
-    jq -r '.artifact_id' ${template_context_path}
-    sleep 1;
-
-    # storePath [https://github.com/fp-mt-test-org/java-kotlin-lib-test-*****]:
-    jq -r '.storePath' ${template_context_path}
     sleep 1;
 
     # java_package_name [javakotlinlibtest*****]:
     jq -r '.java_package_name' ${template_context_path}
     sleep 1;
 
-    # description [*****]:
-    jq -r '.description' ${template_context_path}
-    sleep 1;
-
-    # secrets_artifactory_base_url [*****]:
-    jq -r '.secrets_artifactory_base_url' ${template_context_path}
-    sleep 1;
-
-    # secrets_artifactory_username [*****]:
-    jq -r '.secrets_artifactory_username' ${template_context_path}
-    sleep 1;
-
-    # secrets_artifactory_password [*****]:
-    jq -r '.secrets_artifactory_password' ${template_context_path}
-    sleep 1;
 } | battenberg install "${github_base_url}/${template_name}"
